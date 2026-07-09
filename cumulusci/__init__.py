21938184dfbade91594f824ad28c69bbb45af185
dipakparmar <24366206+dipakparmar@users.noreply.github.com>
2026-07-09 15:57:37 -0700

    DEVOPS-928 chore: fix additional pre-existing flake8 and prettier lint failures

--- cumulusci/__init__.py
+++ cumulusci/__init__.py
@@ -5,7 +5,7 @@ from simple_salesforce import api, bulk
 
 __location__ = os.path.dirname(os.path.realpath(__file__))
 
-from .__about__ import __version__
+from .__about__ import __version__  # noqa: F401
 
 if sys.version_info < (3, 11):  # pragma: no cover
     raise Exception("Clariti CumulusCI requires Python 3.11+.")
--- cumulusci/cli/tests/test_org.py
+++ cumulusci/cli/tests/test_org.py
@@ -1,4 +1,3 @@
-import io
 import json
 from datetime import date, datetime, timedelta
 from pathlib import Path
